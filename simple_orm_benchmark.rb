1c50af28e45c9b4d4e4bb996441b21ab4406441f
Jeremy Evans <code@jeremyevans.net>
2011-05-19 16:30:00 -0700

    Add ruby 1.9 support, and use current ruby for benchmarking all adapters

--- simple_orm_benchmark.rb
+++ simple_orm_benchmark.rb
@@ -3,9 +3,12 @@ require 'rubygems'
 require 'benchmark'
 require 'yaml'
 require 'optparse'
+require 'rbconfig'
+$: << '.'
 
 CONFIG_FILE = 'db.yml'
 ORM_CONFIG = {}
+RUBY=ENV['RUBY'] || File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
 config = nil
 $level = 5
 $disable_gc = false
@@ -47,11 +50,11 @@ end
 configs = opts.permute(*ARGV)
 if $all_configs
   options = ARGV - ["-a"] - configs
-  all_configs.sort.each{|c| system('ruby', __FILE__, *(options + [c]))}
+  all_configs.sort.each{|c| system(RUBY, __FILE__, *(options + [c]))}
   exit
 elsif configs.length > 1
   options = ARGV - configs
-  configs.each{|c| system('ruby', __FILE__, *(options + [c]))}
+  configs.each{|c| system(RUBY, __FILE__, *(options + [c]))}
   exit
 end
 opts.parse!
@@ -66,65 +69,65 @@ $config = ARGV.shift
 require "models/#{parse_config($config)}"
 
 BENCHES=[
-[lambda{"Model Object Creation: #{50*@n} objects"},
+[proc{"Model Object Creation: #{50*@n} objects"},
 nil,
-lambda{(50*@n).times{create_party}}],
+proc{(50*@n).times{create_party}}],
 
-[lambda{"Model Object Select: #{100*@n} objects #{@n} times"},
-lambda{insert_party(100*@n)},
-lambda{@n.times{all_parties}}],
+[proc{"Model Object Select: #{100*@n} objects #{@n} times"},
+proc{insert_party(100*@n)},
+proc{@n.times{all_parties}}],
 
-[lambda{"Model Object Select and Save: #{50*@n} objects"},
-lambda{insert_party(50*@n)},
-lambda{save_all_parties}],
+[proc{"Model Object Select and Save: #{50*@n} objects"},
+proc{insert_party(50*@n)},
+proc{save_all_parties}],
 
-[lambda{"Model Object Destruction: #{100*@n} objects"},
-lambda{insert_party(100*@n); @parties=all_parties},
-lambda{destroy_parties(@parties)}],
+[proc{"Model Object Destruction: #{100*@n} objects"},
+proc{insert_party(100*@n); @parties=all_parties},
+proc{destroy_parties(@parties)}],
 
-[lambda{"Model Object And Associated Object Creation: #{20*@n} objects"},
+[proc{"Model Object And Associated Object Creation: #{20*@n} objects"},
 nil,
-lambda{(20*@n).times{create_party_with_person}}],
+proc{(20*@n).times{create_party_with_person}}],
 
-[lambda{"Model Object and Associated Object Destruction: #{25*@n} objects"},
-lambda{insert_party_people(25*@n, 1)},
-lambda{destroy_parties_and_people}],
+[proc{"Model Object and Associated Object Destruction: #{25*@n} objects"},
+proc{insert_party_people(25*@n, 1)},
+proc{destroy_parties_and_people}],
 
-[lambda{"Eager Loading Query Per Association With 1-1 Records: #{20*@n} objects #{@n*5/7} times"},
-lambda{insert_party_people(20*@n, 1)},
-lambda{(@n*5/7).times{eager_load_party_people}}],
+[proc{"Eager Loading Query Per Association With 1-1 Records: #{20*@n} objects #{@n*5/7} times"},
+proc{insert_party_people(20*@n, 1)},
+proc{(@n*5/7).times{eager_load_party_people}}],
 
-[lambda{"Eager Loading Single Query With 1-1 Records: #{20*@n} objects #{@n*5/7} times"},
-lambda{insert_party_people(20*@n, 1)},
-lambda{(@n*5/7).times{eager_graph_party_people}}],
+[proc{"Eager Loading Single Query With 1-1 Records: #{20*@n} objects #{@n*5/7} times"},
+proc{insert_party_people(20*@n, 1)},
+proc{(@n*5/7).times{eager_graph_party_people}}],
 
-[lambda{"Eager Loading Query Per Association With 1-#{@n} Records: #{@n*@n} objects #{@n*5/7} times"},
-lambda{insert_party_people(@n, @n)},
-lambda{(@n*5/7).times{eager_load_party_people}}],
+[proc{"Eager Loading Query Per Association With 1-#{@n} Records: #{@n*@n} objects #{@n*5/7} times"},
+proc{insert_party_people(@n, @n)},
+proc{(@n*5/7).times{eager_load_party_people}}],
 
-[lambda{"Eager Loading Single Query With 1-#{@n} Records: #{@n*@n} objects #{@n*5/7} times"},
-lambda{insert_party_people(@n, @n)},
-lambda{(@n*5/7).times{eager_graph_party_people}}],
+[proc{"Eager Loading Single Query With 1-#{@n} Records: #{@n*@n} objects #{@n*5/7} times"},
+proc{insert_party_people(@n, @n)},
+proc{(@n*5/7).times{eager_graph_party_people}}],
 
-[lambda{"Eager Loading Query Per Association With 1-#{@n}-#{@n} Records: #{2*@n*@n} objects #{@n*2/7} times"},
-lambda{insert_party_both_people(@n, @n)},
-lambda{(@n*2/7).times{eager_load_party_both_people}}],
+[proc{"Eager Loading Query Per Association With 1-#{@n}-#{@n} Records: #{2*@n*@n} objects #{@n*2/7} times"},
+proc{insert_party_both_people(@n, @n)},
+proc{(@n*2/7).times{eager_load_party_both_people}}],
 
-[lambda{"Eager Loading Single Query With 1-#{@n}-#{@n} Records: #{2*@n*@n} objects 1 time"},
-lambda{insert_party_both_people(@n, @n)},
-lambda{eager_graph_party_both_people}],
+[proc{"Eager Loading Single Query With 1-#{@n}-#{@n} Records: #{2*@n*@n} objects 1 time"},
+proc{insert_party_both_people(@n, @n)},
+proc{eager_graph_party_both_people}],
 
-[lambda{"Lazy Loading With 1-1 Records: #{20*@n} objects 1 time"},
-lambda{insert_party_people(20*@n, 1)},
-lambda{lazy_load_party_people}],
+[proc{"Lazy Loading With 1-1 Records: #{20*@n} objects 1 time"},
+proc{insert_party_people(20*@n, 1)},
+proc{lazy_load_party_people}],
 
-[lambda{"Lazy Loading With 1-#{@n} Records: #{@n*@n} objects #{@n/2} times"},
-lambda{insert_party_people(@n, @n)},
-lambda{(@n/2).times{lazy_load_party_people}}],
+[proc{"Lazy Loading With 1-#{@n} Records: #{@n*@n} objects #{@n/2} times"},
+proc{insert_party_people(@n, @n)},
+proc{(@n/2).times{lazy_load_party_people}}],
 
 ]
 
-thread_block = lambda do
+thread_block = proc do
   threads = []
   @num_threads.round.times do
     threads << Thread.new do
@@ -144,13 +147,13 @@ end
 NO_TRANSACTION_BENCHES = 
 [
 
-[lambda{"Light Threading with #{Math.sqrt(@n).round} threads"},
+[proc{"Light Threading with #{Math.sqrt(@n).round} threads"},
 nil,
-lambda{@num_threads=Math.sqrt(@n).round; instance_eval(&thread_block)}],
+proc{@num_threads=Math.sqrt(@n).round; instance_eval(&thread_block)}],
 
-[lambda{"Heavy Threading with #{@n} threads"},
+[proc{"Heavy Threading with #{@n} threads"},
 nil,
-lambda{@num_threads=@n; instance_eval(&thread_block)}],
+proc{@num_threads=@n; instance_eval(&thread_block)}],
 
 ]
 
@@ -179,7 +182,7 @@ class Bench
     GC.disable if $disable_gc
 
     start_mem = real_memory
-    l = lambda{instance_eval(&@bench)}
+    l = proc{instance_eval(&@bench)}
     @transaction ? transaction(&l) : l.call
     @mem_used = real_memory - start_mem
 
